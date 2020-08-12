5df081e41b429b2c553f96c43cd57acef00d46e5
sald17 <png9981@gmail.com>
2020-08-12 21:34:37 +0700

    update gitignore

--- back-end/app-back-end/node_modules/@loopback/rest/dist/body-parsers/body-parser.json.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, RequestBodyParserOptions } from '../types';
-import { BodyParser, RequestBody } from './types';
-export declare class JsonBodyParser implements BodyParser {
-    name: symbol;
-    private jsonParser;
-    constructor(options?: RequestBodyParserOptions);
-    supports(mediaType: string): boolean;
-    parse(request: Request): Promise<RequestBody>;
-}
--- back-end/app-back-end/node_modules/@loopback/rest/dist/body-parsers/body-parser.text.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, RequestBodyParserOptions } from '../types';
-import { BodyParser, RequestBody } from './types';
-export declare class TextBodyParser implements BodyParser {
-    name: symbol;
-    private textParser;
-    constructor(options?: RequestBodyParserOptions);
-    supports(mediaType: string): boolean;
-    parse(request: Request): Promise<RequestBody>;
-}
